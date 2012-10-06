493e7dfa84c1067811c4a94260f24f2c8718ab90
Jim Posen <jim.posen@gmail.com>
2012-10-06 05:41:28 -0400

    Added KeyBindings class which holds static map of key events mapping.

---
+++ Server/src/com/androidemulatorcontroller/server/KeyBindings.java
@@ -0,0 +1,21 @@
+package com.androidemulatorcontroller.server;
+
+import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyBindings {
+	@SuppressWarnings("serial")
+	public static final Map<Integer, Integer> bindings = new HashMap<Integer, Integer>() {{
+		put(96, KeyEvent.VK_A); // KEYCODE_BUTTON_A
+		put(97, KeyEvent.VK_B); // KEYCODE_BUTTON_B
+		put(98, KeyEvent.VK_C); // KEYCODE_BUTTON_C
+		put(99, KeyEvent.VK_D); // KEYCODE_BUTTON_X
+		put(100, KeyEvent.VK_E); // KEYCODE_BUTTON_Y
+		put(101, KeyEvent.VK_F); // KEYCODE_BUTTON_Z
+		put(102, KeyEvent.VK_G); // KEYCODE_BUTTON_L1
+		put(103, KeyEvent.VK_H); // KEYCODE_BUTTON_R1
+		put(104, KeyEvent.VK_J); // KEYCODE_BUTTON_L2
+		put(105, KeyEvent.VK_H); // KEYCODE_BUTTON_R2
+	}};
+}
