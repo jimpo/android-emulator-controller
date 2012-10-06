7decdfdcde944fc096a7bed46d95600967b418e6
dennis.li.92@gmail.com <don7.sp4m.me@gmail.com>
2012-10-06 03:04:00 -0400

    updated xml and controller selector

--- Client/src/com/androidemulatorcontroller/client/ControllerSelector.java
+++ Client/src/com/androidemulatorcontroller/client/ControllerSelector.java
@@ -3,18 +3,16 @@ package com.androidemulatorcontroller.client;
 import android.os.Bundle;
 import android.view.Menu;
 
-import com.example.asdf.R;
-
 public class ControllerSelector extends BluetoothActivity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.controllerSelector_view);
+        setContentView(R.layout.controllerselector_view);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.controllerSelector_view, menu);
+        getMenuInflater().inflate(R.menu.controllerselector_menu, menu);
         return true;
     }
 
