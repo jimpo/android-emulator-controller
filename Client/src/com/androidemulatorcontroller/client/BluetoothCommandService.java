731bca54fa994f308fb3d0d94a6d1522fe2c5bb7
Jim Posen <jim.posen@gmail.com>
2012-10-06 02:03:25 -0400

    Added back MainActivity.

--- Client/src/com/androidemulatorcontroller/client/BluetoothCommandService.java
+++ Client/src/com/androidemulatorcontroller/client/BluetoothCommandService.java
@@ -65,7 +65,7 @@ public class BluetoothCommandService {
         mState = state;
 
         // Give the new state to the Handler so the UI Activity can update
-        mHandler.obtainMessage(RemoteBluetooth.MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
+        mHandler.obtainMessage(BluetoothActivity.MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
     }
 
     /**
@@ -129,9 +129,9 @@ public class BluetoothCommandService {
         mConnectedThread.start();
 
         // Send the name of the connected device back to the UI Activity
-        Message msg = mHandler.obtainMessage(RemoteBluetooth.MESSAGE_DEVICE_NAME);
+        Message msg = mHandler.obtainMessage(BluetoothActivity.MESSAGE_DEVICE_NAME);
         Bundle bundle = new Bundle();
-        bundle.putString(RemoteBluetooth.DEVICE_NAME, device.getName());
+        bundle.putString(BluetoothActivity.DEVICE_NAME, device.getName());
         msg.setData(bundle);
         mHandler.sendMessage(msg);
 
@@ -190,9 +190,9 @@ public class BluetoothCommandService {
         setState(STATE_LISTEN);
 
         // Send a failure message back to the Activity
-        Message msg = mHandler.obtainMessage(RemoteBluetooth.MESSAGE_TOAST);
+        Message msg = mHandler.obtainMessage(BluetoothActivity.MESSAGE_TOAST);
         Bundle bundle = new Bundle();
-        bundle.putString(RemoteBluetooth.TOAST, "Unable to connect device");
+        bundle.putString(BluetoothActivity.TOAST, "Unable to connect device");
         msg.setData(bundle);
         mHandler.sendMessage(msg);
     }
@@ -204,9 +204,9 @@ public class BluetoothCommandService {
 //        mConnectionLostCount++;
 //        if (mConnectionLostCount < 3) {
 //        	// Send a reconnect message back to the Activity
-//	        Message msg = mHandler.obtainMessage(RemoteBluetooth.MESSAGE_TOAST);
+//	        Message msg = mHandler.obtainMessage(BluetoothActivity.MESSAGE_TOAST);
 //	        Bundle bundle = new Bundle();
-//	        bundle.putString(RemoteBluetooth.TOAST, "Device connection was lost. Reconnecting...");
+//	        bundle.putString(BluetoothActivity.TOAST, "Device connection was lost. Reconnecting...");
 //	        msg.setData(bundle);
 //	        mHandler.sendMessage(msg);
 //	        
@@ -214,9 +214,9 @@ public class BluetoothCommandService {
 //        } else {
         	setState(STATE_LISTEN);
 	        // Send a failure message back to the Activity
-	        Message msg = mHandler.obtainMessage(RemoteBluetooth.MESSAGE_TOAST);
+	        Message msg = mHandler.obtainMessage(BluetoothActivity.MESSAGE_TOAST);
 	        Bundle bundle = new Bundle();
-	        bundle.putString(RemoteBluetooth.TOAST, "Device connection was lost");
+	        bundle.putString(BluetoothActivity.TOAST, "Device connection was lost");
 	        msg.setData(bundle);
 	        mHandler.sendMessage(msg);
 //        }
@@ -326,7 +326,7 @@ public class BluetoothCommandService {
                     int bytes = mmInStream.read(buffer);
 
                     // Send the obtained bytes to the UI Activity
-                    mHandler.obtainMessage(RemoteBluetooth.MESSAGE_READ, bytes, -1, buffer)
+                    mHandler.obtainMessage(BluetoothActivity.MESSAGE_READ, bytes, -1, buffer)
                             .sendToTarget();
                 } catch (IOException e) {
                     Log.e(TAG, "disconnected", e);
---
+++ Client/src/com/androidemulatorcontroller/client/MainActivity.java
@@ -0,0 +1,38 @@
+package com.androidemulatorcontroller.client;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+	private static final int REQUEST_CONNECT_DEVICE = 1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case R.id.scan:
+            // Launch the DeviceListActivity to see devices and do scan
+            Intent serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+            return true;
+        }
+        return false;
+    }
+
+}
